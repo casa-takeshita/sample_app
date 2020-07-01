76b67ab9b00da2b8bf9e871189e06218a4c02b22
casa-a-takeshita <asuka.takeshita@casa-inc.co.jp>
2020-07-01 11:26:12 +0900

    fix test code
    fix test code

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,13 +2,13 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-    def setup
-      @base_title = "Ruby on Rails Tutorial Sample App"
+    test "should get root" do
+      get static_pages_home_url
+      assert_response :success
     end
 
-    test "should get root" do
-      get FILL_IN
-      assert_response :FILL_IN
+    def setup
+      @base_title = "Ruby on Rails Tutorial Sample App"
     end
 
     test "should get home" do
